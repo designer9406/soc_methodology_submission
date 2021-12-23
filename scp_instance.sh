2d45d97847d9c6c769aab07d81748a6e5262c9fa
hongjunum <designer9406@gmail.com>
2021-12-24 06:11:40 +0900

    scp_instance.sh modified

--- scp_instance.sh
+++ scp_instance.sh
@@ -5,7 +5,6 @@ if [ $# -ne 1 ]; then
  exit -1
 fi
 
-FIRESIM_KEYS_LOCATION=$1
-MANAGER_INSTANCE_IP=$2
+MANAGER_INSTANCE_IP=$1
 
 scp -i firesim.pem -r ./firesim.pem  ./firesim_config  ./hwacha-net  ./instance_setup.sh centos@$1:/home/centos
